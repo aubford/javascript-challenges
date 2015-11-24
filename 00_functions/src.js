11ed4b2e79b305eff367787952002a9f6c74c822
Aubrey Ford <musicaubrey@gmail.com>
2015-11-24 15:02:11 -0700

    challenge 1 compelete

--- 00_functions/src.js
+++ 00_functions/src.js
@@ -1,12 +1,15 @@
 module.exports = {
 
   helloWorld: function(){
+      return "Hello World"
   },
 
   hello: function(string){
+    return ("Hello " + string)
   },
 
-  shout: function() {
+  shout: function(string) {
+    return ("Hello " + string.toUpperCase())
 
   }
 }
