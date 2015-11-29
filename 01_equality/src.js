ab9211fa52f9d0ca42cb52c29879c57312764d1c
Aubrey Ford <musicaubrey@gmail.com>
2015-11-28 17:41:36 -0700

    completed 01

--- 01_equality/src.js
+++ 01_equality/src.js
@@ -1,5 +1,44 @@
 module.exports = {
   sameLength: function(string1, string2) {
     // your code here:
-  }
+
+    return string1.length === string2.length
+
+  },
+
+  firstGreater: function(a, b) {
+      return a > b
+    },
+
+
+    firstGreaterOrEqualTo: function(a,b) {
+        return a >= b
+    },
+
+    firstLengthGreater: function(a,b) {
+      return a.length > b.length
+    },
+
+    secondGreater: function(a,b) {
+      return a < b
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
