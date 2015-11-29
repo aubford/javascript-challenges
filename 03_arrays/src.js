9a22a5ccf5d4cf8bb97d129333d275cbd7e76279
Aubrey Ford <musicaubrey@gmail.com>
2015-11-28 18:39:50 -0700

    completed 03

--- 03_arrays/src.js
+++ 03_arrays/src.js
@@ -1,7 +1,75 @@
 module.exports = {
 
   first: function(array) {
-    // fill in code here
-  }
+      return array[0]
+  },
+
+  last: function(array) {
+    return array[array.length-1]
+  },
+
+  empty: function(array) {
+    if (array.length === 0){
+      return true
+    } else {
+      return false
+    }
+  },
+
+  first_n: function(array,n) {
+        if (array.length > n) {
+        array.splice(n, array.length - n)
+        }
+        return array
+  },
+
+  last_n: function(array,n) {
+        if (array.length > n) {
+        array.splice(0, n-1)
+        }
+        return array
+  },
+
+
+    drop: function(array,n) {
+          var contain = []
+          for (var i = array.length - 1; i >= n; i--) {
+            contain.push(array[i])
+          }
+          return contain.reverse()
+    },
+
+    union: function(array1, array2) {
+
+        for (var i = 0; i < array2.length; i++) {
+          array1.push(array2[i])
+        }
+          return array1
+
+    },
+
+    intersection: function(array1, array2) {
+      var container =[]
+      for (var i = 0; i < array1.length; i++) {
+        for (var a = 0; a < array2.length; a++) {
+          if (array2[a] === array1[i]) {
+            container.push(array1[i])
+          }
+        }
+      }
+      return container
+    },
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
