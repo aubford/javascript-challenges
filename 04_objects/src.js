f04f5473aead98c53aebbe4d5d9a41d0ff23c79a
Aubrey Ford <musicaubrey@gmail.com>
2015-11-28 22:10:42 -0700

    completed 06

--- 04_objects/src.js
+++ 04_objects/src.js
@@ -1,5 +1,64 @@
 module.exports = {
   person: function() {
-    // your code here
-  }
+    var person = {
+      firstName: 'John',
+      lastName: 'Mayer'
+    }
+    return person
+  },
+
+
+  sweetnessProperty: function(object) {
+    return object.sweetness
+  },
+
+
+  savorynessProperty: function(ob) {
+      return ob["savory-ness"]
+  },
+
+  keys: function(ob) {
+    var container = []
+     for (each in ob) {
+      container.push(each)
+    }
+    return container
+  },
+
+  sortedKeys: function(ob) {
+    var container = []
+    for (each in ob) {
+      container.push(each)
+    }
+    return container.sort()
+  },
+
+  reverseSortedKeys: function(ob) {
+    var container = []
+    for (each in ob) {
+      container.push(each)
+    }
+    return container.sort().reverse()
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- 06_join_for_loop/src.js
+++ 06_join_for_loop/src.js
@@ -5,7 +5,63 @@
 module.exports = {
 
   joinWithFor: function(list) {
-    // your code here
+    return list.join("")
+  },
+
+  joinWithForAndIndex: function(array) {
+    string = []
+    for (var i = 0; i < array.length; i++) {
+        string.push(array[i])
+        string.push(i)
+    }
+    return string.join("")
+  },
+
+  joinWithoutOddCharacters: function (array) {
+    string=[]
+    for (var i = 0; i < array.length; i+=2) {
+      string.push(array[i])
+    }
+    return string.join("")
+  },
+
+  joinWithForAndToken: function(array, t) {
+    return array.join(t)
+  },
+
+  joinWithForAndAlternatingTokens: function (array, t, j) {
+    container=[]
+    for (var i = 0; i < array.length; i++) {
+        container.push(array[i])
+
+        if (i === array.length-1){
+        } else if (array[i] % 2 === 0) {
+          container.push('+')
+        } else {
+          container.push('*')
+        }
+    }
+    return container.join('')
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
