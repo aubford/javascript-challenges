76304b8e619d506d01db45f37e1461c74899869f
Aubrey Ford <musicaubrey@gmail.com>
2015-11-28 23:27:28 -0700

    compeleted 09

--- 09_recursion/src.js
+++ 09_recursion/src.js
@@ -34,7 +34,14 @@ module.exports = {
 
   factorial: function(n) {
 
-    
+      function fact(a) {
+          if (a === 0) {
+            return 1
+          } else {
+            return a * fact(a-1)
+          }
 
+      }
+      return fact(n)
   }
 }
